56866887330b826168cea1ec30004afb006f2723
ester <ester@ZETTERLUND>
2023-09-06 15:50:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloEdugrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
